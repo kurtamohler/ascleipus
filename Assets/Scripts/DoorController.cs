ef4b817ae9c191b18c83bc52c057a922390d6c38
Kurt Mohler <kurtamohler@gmail.com>
2019-10-06 15:54:56 -0500

    Added doors that can be opened with buttons

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> buttons;
+    [SerializeField] private Vector3 openOffset;
+    [SerializeField] private float speed;
+
+    private float acceptableDistance = 0.001f;
+
+    private Vector3 closedPosition;
+    private Vector3 openPosition;
+
+    bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        closedPosition = this.transform.position;
+        openPosition = closedPosition + openOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttons.Count > 0) {
+            CheckButtons();
+        }
+        UpdatePosition();
+    }
+
+    void CheckButtons() {
+        bool allButtonsPressed = true;
+
+        foreach (GameObject button in buttons) {
+            if (!button.GetComponent<ButtonController>().IsPressed()) {
+                allButtonsPressed = false;
+            }
+        }
+
+        open = allButtonsPressed;
+    }
+
+    void UpdatePosition() {
+        if (open) {
+            MoveToPosition(openPosition);
+
+        } else {
+            MoveToPosition(closedPosition);
+
+        }
+    }
+
+    void MoveToPosition(Vector3 position) {
+        if ((transform.position - position).magnitude > acceptableDistance) {
+                transform.position = Vector3.Slerp(transform.position, position, speed*Time.deltaTime);
+        }
+    }
+}
